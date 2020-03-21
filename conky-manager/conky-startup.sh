d63d183727f45d07311e09b8599e3f3230675423
Manuel <manuel.simon.novoa@gmail.com>
2020-03-21 18:51:12 +0100

    Update commands to a general user
    Not only root. It should work with every user.

--- conky-manager/conky-startup.sh
+++ conky-manager/conky-startup.sh
@@ -1,13 +1,13 @@
 killall conky
-cd "/root/conky-manager/themes/Metro"
-conky -c "/root/conky-manager/themes/Metro/config/conky.conf.12" &
-cd "/root/conky-manager/themes/Metro"
-conky -c "/root/conky-manager/themes/Metro/config/conky.conf.13" &
-cd "/root/conky-manager/themes/Metro"
-conky -c "/root/conky-manager/themes/Metro/config/conky.conf.14" &
-cd "/root/conky-manager/themes/Metro"
-conky -c "/root/conky-manager/themes/Metro/config/conky.conf.23" &
-cd "/root/conky-manager/themes/Metro"
-conky -c "/root/conky-manager/themes/Metro/config/conky.conf.25" &
-cd "/root/conky-manager/themes/verti-color"
-conky -c "/root/conky-manager/themes/verti-color/config/cpu_8core" &
+cd "$HOME/conky-manager/themes/Metro"
+conky -c "$HOME/conky-manager/themes/Metro/config/conky.conf.12" &
+cd "$HOME/conky-manager/themes/Metro"
+conky -c "$HOME/conky-manager/themes/Metro/config/conky.conf.13" &
+cd "$HOME/conky-manager/themes/Metro"
+conky -c "$HOME/conky-manager/themes/Metro/config/conky.conf.14" &
+cd "$HOME/conky-manager/themes/Metro"
+conky -c "$HOME/conky-manager/themes/Metro/config/conky.conf.23" &
+cd "$HOME/conky-manager/themes/Metro"
+conky -c "$HOME/conky-manager/themes/Metro/config/conky.conf.25" &
+cd "$HOME/conky-manager/themes/verti-color"
+conky -c "$HOME/conky-manager/themes/verti-color/config/cpu_8core" &
